4bc20076e8dec90934d96abe6dfd86142e7eab6d
rustylocks79 <jeremydellock570@gmail.com>
2020-07-19 13:10:53 -0400

    cleaning output.

--- main.py
+++ main.py
@@ -9,14 +9,11 @@ from figgie import Figgie
 from generators import isg_basic, isg_abstract, ag_basic, ag_abstract, am_abstract
 
 
-def save(strategy: dict, trials: int, info_set_method: str):
+def save(strategy: dict, trials: int, info_set_method: str) -> str:
     file_name = 'strategies/strategy_{}_{}.pickle'.format(trials, info_set_method)
-    print('Saving to {}: '.format(file_name))
-    start_time = time.process_time()
     with open(file_name, 'wb') as file:
         pickle.dump(strategy, file)
-    total_time = time.process_time() - start_time
-    print('Saving to file took {} \n'.format(total_time))
+    return file_name
 
 
 def load(file_name: str) -> dict:
@@ -30,26 +27,19 @@ def load(file_name: str) -> dict:
 
 
 def play(game: Figgie, agents: list, games: int):
-    print('Testing: ')
     game.reset()
     start_time = time.process_time()
     game.play(agents, games)
     total_time = time.process_time() - start_time
-    print('\ttime: {} seconds'.format(total_time))
+    print('\tTesting took {} seconds'.format(total_time))
 
-    print('Results: ')
+    print('\tResults: ')
     for i, agent in enumerate(agents):
-        print('agent {}'.format(i))
-        print('\tag: {}'.format(agent.action_generator.__name__))
-        if agent.action_mapper is not None:
-            print('\tam: {}'.format(agent.action_mapper.__name__))
-        print('\twins: {}'.format(agent.wins))
-        print('\ttotal utility: {}'.format(agent.total_utility))
-        print('\tavg. utility: {}'.format(agent.total_utility / games))
+        print('\tagent {}'.format(i))
+        print('\t\twins: {}'.format(agent.wins))
+        print('\t\tavg. utility: {}, total utility: {}'.format(agent.total_utility / games, agent.total_utility))
         if isinstance(agent, StrategyAgent):
-            print('\tisg: {}'.format(agent.info_set_generator.__name__))
-            print('\tunknown states: {}'.format(agent.unknown_states))
-            print('\tavg unknown states: {}'.format(agent.unknown_states / games))
+            print('\t\tavg unknown states: {}, unknown states: {}'.format(agent.unknown_states / games, agent.unknown_states))
 
 
 def main():
@@ -70,26 +60,45 @@ def main():
     print('\tinfo set generator: {}'.format(minimizer.action_mapper.__name__))
     print()
 
-    for i in range(args.iterations):
-        print('Training: ')
+    agents = [RandomAgent(ag_basic),
+              RandomAgent(ag_basic),
+              RandomAgent(ag_basic),
+              StrategyAgent(None, isg_basic, ag_abstract, am_abstract)]
+
+    for i, agent in enumerate(agents):
+        print('agent {} ({})'.format(i, type(agent)))
+        if isinstance(agent, StrategyAgent):
+            print('\tisg: {}'.format(agent.info_set_generator.__name__))
+        print('\tag: {}'.format(agent.action_generator.__name__))
+        if agent.action_mapper is not None:
+            print('\tam: {}'.format(agent.action_mapper.__name__))
+    print()
+
+    for i in range(1, args.iterations + 1):
+        print('Iteration: {}'.format(i))
+
         start_time = time.process_time()
         minimizer.train(args.trials)
+        total_time = time.process_time() - start_time
+        print('\tTraining took {} seconds '.format(total_time))
+
+        start_time = time.process_time()
         strategy = minimizer.get_strategy()
         total_time = time.process_time() - start_time
-        print('\ttime: {} seconds'.format(total_time))
+        print('\tExtracting took {} seconds'.format(total_time))
 
-        print('Strategy: ')
-        print('\tinfo sets: {}'.format(len(strategy)))
-        print('\tsize: {} bytes\n'.format(sys.getsizeof(strategy)))
+        print('\tStrategy: ')
+        print('\t\tinfo sets: {}'.format(len(strategy)))
+        print('\t\tsize: {} bytes'.format(sys.getsizeof(strategy)))
 
-        save(strategy, args.trials * (i + 1), 'basic')
+        start_time = time.process_time()
+        file_name = save(strategy, args.trials * i, 'basic')
+        total_time = time.process_time() - start_time
+        print('\tSaving to {} took {} seconds'.format(file_name, total_time))
 
-        agents = [RandomAgent(ag_basic),
-                  RandomAgent(ag_basic),
-                  RandomAgent(ag_basic),
-                  StrategyAgent(strategy, isg_basic, ag_abstract, am_abstract)]
-
+        agents[3].strategy = strategy
         play(game, agents, args.games)
+        print('---------------------------------------------------')
 
 
 if __name__ == '__main__':
