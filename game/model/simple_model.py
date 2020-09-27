99f652dddf08711754bdc120124684abd91fc993
rustylocks79 <jeremydellock570@gmail.com>
2020-09-27 08:58:11 -0400

    added numba

--- game/model/simple_model.py
+++ game/model/simple_model.py
@@ -1,3 +1,5 @@
+from numba import njit
+
 from game.figgie import Figgie
 from game.model.utility_model import UtilityModel
 from game.suit import Suit, SUITS
@@ -5,9 +7,7 @@ from game.suit import Suit, SUITS
 PROBABILITY_8_CARD_GOAL_SUIT = .33
 PROBABILITY_10_CARD_GOAL_SUIT = .67
 
-
 class SimpleModel(UtilityModel):
-
     def get_card_utility(self, figgie: Figgie, index: int, suit: Suit) -> float:
         hand = figgie.cards[index]
         for s in SUITS:
@@ -21,7 +21,8 @@ class SimpleModel(UtilityModel):
         return max(result, 0)
 
     @staticmethod
-    def get_expected_from_pot(cards: int):
+    @njit()
+    def get_expected_from_pot(cards: int) -> float:
         """
         :param cards: the number of cards the agent has of one suit
         :return: the expected utility the agent will receive from the pot should this suit be the goal suit.
