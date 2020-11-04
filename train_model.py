999976a7f25f10cc8241d08e536c1b9e182fc63b
rustylocks79 <jeremydellock570@gmail.com>
2020-11-04 10:23:46 -0500

    added raytune.

--- train_model.py
+++ train_model.py
@@ -1,7 +1,11 @@
 import argparse
 import pickle
 
+import numpy as np
+import ray
 import torch.optim
+from ray import tune
+from ray.tune.schedulers import ASHAScheduler
 from torch.nn import Module, Linear, CrossEntropyLoss
 from torch.nn.functional import relu, nll_loss, log_softmax
 from torch.optim import Adam
@@ -11,13 +15,24 @@ INPUT_SIZE = 24
 OUTPUT_SIZE = 4
 
 
+class FiggieDataSet(Dataset):
+    def __init__(self, samples) -> None:
+        self.samples = samples
+
+    def __getitem__(self, index: int):
+        return self.samples[index]
+
+    def __len__(self) -> int:
+        return len(self.samples)
+
+
 class Net(Module):
-    def __init__(self):
+    def __init__(self, l1: int, l2: int, l3: int):
         super().__init__()
-        self.fc1 = Linear(INPUT_SIZE, 64)
-        self.fc2 = Linear(64, 64)
-        self.fc3 = Linear(64, 64)
-        self.fc4 = Linear(64, OUTPUT_SIZE)
+        self.fc1 = Linear(INPUT_SIZE, l1)
+        self.fc2 = Linear(l1, l2)
+        self.fc3 = Linear(l2, l3)
+        self.fc4 = Linear(l3, OUTPUT_SIZE)
 
     def forward(self, x):
         x = relu(self.fc1(x))
@@ -27,60 +42,70 @@ class Net(Module):
         return x
 
 
-if __name__ == '__main__':
-    parser = argparse.ArgumentParser(description='Train a strategy using CFR')
-    parser.add_argument('-in', '--input', type=str, help='the data')
-    parser.add_argument('-out', '--output', type=str, help='where to store the model')
-    args = parser.parse_args()
-    with open(args.input, 'rb') as file:
-        all_data = pickle.load(file)
+def train(model, optimizer, loss, train_loader):
+    model.train()
+    for batch_idx, (x, y) in enumerate(train_loader):
+        model.zero_grad()
+        output = model(x.view(-1, INPUT_SIZE))
+        loss = nll_loss(output, y)
+        loss.backward()
+        optimizer.step()
 
 
-    class FiggieDataSet(Dataset):
-        def __init__(self, samples) -> None:
-            self.samples = samples
-
-        def __getitem__(self, index: int):
-            return self.samples[index]
-
-        def __len__(self) -> int:
-            return len(self.samples)
-
-
-    point = int(9/10 * len(all_data))
-    train = FiggieDataSet(all_data[:point])
-    test = FiggieDataSet(all_data[point:])
-    train_set = DataLoader(train, batch_size=20, shuffle=True)
-    test_set = DataLoader(test, batch_size=20, shuffle=False)
-
-    net = Net()
-    loss_function = CrossEntropyLoss()
-    optimizer = Adam(net.parameters(), lr=0.001)
-
-    for epoch in range(3):
-        loss = 0
-        for data in train_set:
-            X, y = data
-            net.zero_grad()
-            output = net(X.view(-1, INPUT_SIZE))
-            loss = nll_loss(output, y)
-            loss.backward()
-            optimizer.step()
-        print(loss)
-
+def test(model, data_loader):
+    model.eval()
     correct = 0
     total = 0
-
     with torch.no_grad():
-        for data in test_set:
-            X, y = data
-            output = net(X.view(-1, INPUT_SIZE))
-            output = torch.exp(output)
+        for batch_idx, (x, y) in enumerate(data_loader):
+            output = model(x.view(-1, INPUT_SIZE))
             for idx, i in enumerate(output):
                 if torch.argmax(i) == y[idx]:
                     correct += 1
                 total += 1
+    return correct / total
 
-    print("Accuracy: ", round(correct/total, 3))
 
-    torch.save(net.state_dict(), args.output)
+def train_figgie(config, checkpoint_dir=None):
+    with open(r"C:\CompSci\Figgie\ann\training_data.pickle", 'rb') as file:
+        all_data = pickle.load(file)
+    point = int(9 / 10 * len(all_data))
+    train_data = FiggieDataSet(all_data[:point])
+    test_data = FiggieDataSet(all_data[point:])
+    train_set = DataLoader(train_data, batch_size=64, shuffle=True)
+    test_set = DataLoader(test_data, batch_size=64, shuffle=False)
+
+    model = Net(config['l1'], config['l2'], config['l3'])
+    loss_function = CrossEntropyLoss()
+    optimizer = Adam(model.parameters(), lr=config['lr'])
+
+    for epoch in range(10):
+        train(model, optimizer, loss_function, train_set)
+        acc = test(model, test_set)
+        tune.report(mean_accuracty=acc)
+
+        if epoch % 5 == 0:
+            torch.save(model.state_dict(), r"C:\CompSci\Figgie\ann\model_{}_{}_{}_{}.pth"
+                       .format(config['l1'], config['l2'], config['l3'], acc))
+
+def main():
+    parser = argparse.ArgumentParser(description='Train a strategy using CFR')
+    parser.add_argument('-in', '--input', type=str, help='the data')
+    parser.add_argument('-out', '--output', type=str, help='where to store the model')
+    args = parser.parse_args()
+
+    config = {
+        "l1": tune.sample_from(lambda _: 2 ** np.random.randint(2, 9)),
+        "l2": tune.sample_from(lambda _: 2 ** np.random.randint(2, 9)),
+        "l3": tune.sample_from(lambda _: 2 ** np.random.randint(2, 9)),
+        "lr": tune.loguniform(1e-4, 1e-1),
+    }
+    ray.init(num_cpus=1, include_dashboard=False, local_mode=True)
+    scheduler = ASHAScheduler(metric='mean_accuracy', mode='max')
+    analysis = tune.run(train_figgie,
+                        num_samples=4,
+                        scheduler= scheduler,
+                        config=config)
+
+if __name__ == '__main__':
+    main()
