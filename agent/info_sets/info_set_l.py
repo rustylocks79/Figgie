a714f79980425e91a46076988c79eab69bf2b998
rustylocks79 <jeremydellock570@gmail.com>
2020-11-11 11:28:39 -0500

    bug fix.

--- agent/info_sets/info_set_l.py
+++ agent/info_sets/info_set_l.py
@@ -8,4 +8,4 @@ class InfoSetL(InfoSetStd):
 
     def generate_info_set(self, figgie: Figgie, card_util: int, target_operation: str, target_suit: Suit):
         last_transaction = figgie.markets[target_suit.value].last_price
-        return super().generate_info_set(figgie, card_util, target_operation, target_suit) + ',' + last_transaction
+        return super().generate_info_set(figgie, card_util, target_operation, target_suit) + ',' + str(last_transaction)
