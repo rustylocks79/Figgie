71cba452eb5cabc035aa878d48f51e8f0c558b1f
rustylocks79 <jeremydellock570@gmail.com>
2021-01-20 10:55:19 -0500

    correction for ot.

--- agent/info_sets/info_set_ot.py
+++ agent/info_sets/info_set_ot.py
@@ -4,8 +4,8 @@ from figgie import Figgie, Suit
 
 class InfoSetOT(InfoSetStd):
     def __init__(self):
-        super().__init__('ol')
+        super().__init__('ot')
 
     def generate_info_set(self, figgie: Figgie, card_util: int, target_operation: str, target_suit: Suit):
         market = figgie.markets[target_suit.opposite().value]
-        return super().generate_info_set(figgie, card_util, target_operation, target_suit) + ',' + str(market.tranactions)
+        return super().generate_info_set(figgie, card_util, target_operation, target_suit) + ',' + str(market.transactions)
